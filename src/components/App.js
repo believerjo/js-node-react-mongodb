24ca6e49b41e813910c771b638113154b5b64ffb
tjrm <believerjo@gmail.com>
2018-03-26 16:39:20 +1100

    components done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div>
-      <Header message="Naming Contests" />
+class App extends React.Component {
+  state = {
+    pageHeader : 'Naming Contests'
+  };
+  componentDidMount(){
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    return (
       <div>
+        <Header message={this.state.pageHeader} />
+        <div>
         ...
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
